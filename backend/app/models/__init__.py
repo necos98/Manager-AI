f2a621dce0177f2dfc2e3b20a76b1e6649b8bc0e
Jacob <jacob@example.com>
2026-03-28 19:38:14 +0100

    feat: ProjectVariable model and migration

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -5,6 +5,7 @@ from app.models.issue_feedback import IssueFeedback
 from app.models.project import Project
 from app.models.project_file import ProjectFile
 from app.models.project_skill import ProjectSkill
+from app.models.project_variable import ProjectVariable
 from app.models.prompt_template import PromptTemplate
 from app.models.setting import Setting
 from app.models.task import Task
@@ -12,5 +13,5 @@ from app.models.terminal_command import TerminalCommand
 
 __all__ = [
     "ActivityLog", "Base", "Issue", "IssueFeedback", "Project", "ProjectFile",
-    "ProjectSkill", "PromptTemplate", "Setting", "Task", "TerminalCommand",
+    "ProjectSkill", "ProjectVariable", "PromptTemplate", "Setting", "Task", "TerminalCommand",
 ]
